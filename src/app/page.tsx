adc4e1f9ba0ee0e39b3f51e0a36318a02b90d034
Rish1369 <rishabhrai5649@gmail.com>
2025-07-27 00:58:04 +0530

    done with customisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
+
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-rose-700 text-7xl">
-        Hello world
-      </p>
+      <div className="p-4">
+          <div><Button variant={"elevated"}>
+            I am a button
+          </Button></div>
+          <Input placeholder="i am input">
+          </Input>
+          <Progress value={50}>
+          </Progress>
+          <Textarea placeholder="i am text area">
+          </Textarea>
+          <Checkbox>
+          </Checkbox>
+      </div>
     </div>
   );
 }
